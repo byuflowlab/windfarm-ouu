b9456eadd41328556ebc2a4cc1e0cda04dffadcc
Jared J. Thomas <jaredthomas68@gmail.com>
2016-02-03 09:54:52 -0700

    made OUU dependent on current FLORIS repository via setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+import platform
+
+
+setup(
+    name='OUUWakeModels',
+    version='0.0.0',
+    description='collection of wake models for optimization under uncertainty',
+    install_requires=['florisse'],
+    dependency_links=['https://github.com/WISDEM/FLORISSE.git/tarball/FLORIScosine#egg=florisse'],
+    zip_safe=False
+)
