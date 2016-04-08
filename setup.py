2418024905ec53841368510f9ba16b8dca658770
PJ Stanley <stanley_andrewpj@yahoo.com>
2016-04-08 11:47:48 -0600

    setup change

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(
     	version='0.0.0',
     	description='collection of wake models for optimization under uncertainty',
     	install_requires=['florisse', 'akima>=1.0'],
-    	dependency_links=['https://github.com/WISDEM/FLORISSE/tarball/FLORIScosine#egg=florisse', 'https://github.com/andrewning/akima/tarball/master#egg=akima-1.0.0'],
+    	dependency_links=['https://github.com/WISDEM/FLORISSE/tarball/develop#egg=florisse', 'https://github.com/andrewning/akima/tarball/master#egg=akima-1.0.0'],
 	zip_safe=False
 )
