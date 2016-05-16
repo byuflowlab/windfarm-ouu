Fixing the dakota gradients

1d_dakota_uniform/statisticsComponents.py
@@ -20,9 +20,11 @@ class DakotaStatistics(ExternalCode):
 
         # define inputs
         self.add_param('power', np.zeros(nDirections), units ='kW',
-                       desc = 'vector containing the power production at each wind direction ccw from north')
+                       desc='vector containing the power production for each winddirection and windspeed pair')
+        self.add_param('method_dict', method_dict,
+                       desc='parameters for the UQ method')
         self.add_param('weights', np.zeros(nDirections),
-                       desc = 'vector containing the integration weight associated with each power')
+                       desc='vector containing the integration weight associated with each power')
 
         # define output
         self.add_output('mean', val=0.0, units='kWh', desc='mean annual energy output of wind farm')
@@ -36,9 +38,6 @@ class DakotaStatistics(ExternalCode):
 
         # Generate the file with the power vector for Dakota
         power = params['power']
-        weights = params['weights']
-        # power = power*weights
-        power = power
         np.savetxt('powerInput.txt', power, header='power')
 
         # parent solve_nonlinear function actually runs the external code
@@ -56,8 +55,9 @@ class DakotaStatistics(ExternalCode):
 
     def linearize(self, params, unknowns, resids):
 
-        # Todo update linearize
         J = linearize_function(params)
+        # print('Calculate Derivatives:', self.name)
+
         return J
 
 
@@ -142,17 +142,18 @@ class RectStatistics(Component):
         super(RectStatistics, self).__init__()
 
         # set finite difference options (fd used for testing only)
+        # self.fd_options['force_fd'] = True
         self.fd_options['form'] = 'central'
         self.fd_options['step_size'] = 1.0e-5
         self.fd_options['step_type'] = 'relative'
 
         # define inputs
         self.add_param('power', np.zeros(nDirections), units ='kW',
-                       desc = 'vector containing the power production at each wind direction ccw from north')
+                       desc='vector containing the power production for each winddirection and windspeed pair')
         self.add_param('method_dict', method_dict,
-                       desc = 'parameters for the UQ method')
+                       desc='parameters for the UQ method')
         self.add_param('weights', np.zeros(nDirections),
-                       desc = 'vector containing the integration weight associated with each power')
+                       desc='vector containing the integration weight associated with each power')
 
         # define output
         self.add_output('mean', val=0.0, units='kWh', desc='mean annual energy output of wind farm')
@@ -161,11 +162,7 @@ class RectStatistics(Component):
     def solve_nonlinear(self, params, unknowns, resids):
 
         power = params['power']
-        n = len(power)
-        method_dict = params['method_dict']
-        # unused, weights = quadrature_rules.rectangle(n, method_dict['distribution'])
         weights = params['weights']
-        # print 'aloha', weights
 
         mean = sum(power*weights)
         # Calculate std to ensure it is positive, first method could have issues for small number of samples
@@ -178,62 +175,69 @@ class RectStatistics(Component):
         # promote statistics to class attribute
         unknowns['mean'] = mean*hours
         unknowns['std'] = std*hours
-        print weights
-        print unknowns['mean']
+        # print weights
+        # print unknowns['mean']
 
         print 'In RectStatistics'
 
     def linearize(self, params, unknowns, resids):
 
         J = linearize_function(params)
+        # print('Calculate Derivatives:', self.name)
         return J
 
 
 def linearize_function(params):
 
-    power = params['power']
-    n = len(power)
-    method_dict = params['method_dict']
-    dist = method_dict['distribution']
-    unused, weights = quadrature_rules.rectangle(n, method_dict['distribution'])
-
-    # I just added this
     weights = params['weights']
 
     # number of hours in a year
     hours = 8760.0
-    dAEP_dpower = weights*hours
+    dmean_dpower = weights*hours
 
     J = {}
-    J[('mean', 'power')] = np.array([dAEP_dpower])
+    J[('mean', 'power')] = np.array([dmean_dpower])
 
     return J
 
 
 if __name__ == "__main__":
 
-    from WindFreqFunctions import wind_direction_pdf
-    dakotaFileName = 'dakotaAEPdirection.in'
-    winddirections, weights = getSamplePoints(dakotaFileName)
-    f = wind_direction_pdf()
-    rho = f(winddirections)
+    import distributions
+    from statistics_convergence import getPoints
+    method_dict = {}
+    method_dict['method']           = 'rect'
+    method_dict['uncertain_var']    = 'direction'
+    method_dict['layout']           = 'optimized'
+
+    if method_dict['uncertain_var'] == 'speed':
+        dist = distributions.getWeibull()
+        method_dict['distribution'] = dist
+    elif method_dict['uncertain_var'] == 'direction':
+        dist = distributions.getWindRose()
+        method_dict['distribution'] = dist
+    else:
+        raise ValueError('unknown uncertain_var option "%s", valid options "speed" or "direction".' %method_dict['uncertain_var'])
+
+    method_dict['dakota_filename'] = 'dakotageneral.in'
+
+    n = 10
+    unused, weights = getPoints(method_dict, n)
     prob = Problem(root=Group())
-    prob.root.add('p', IndepVarComp('power', np.random.rand(10)))
+    prob.root.add('p', IndepVarComp('power', np.random.rand(n)))
     prob.root.add('w', IndepVarComp('weight', weights))
-    prob.root.add('rho', IndepVarComp('frequency', rho))
-    # prob.root.add('DakotaAEP', DakotaAEP(dakotaFileName=dakotaFileName))
-    prob.root.add('DakotaAEP', SimpleAEP())
-    prob.root.connect('p.power', 'DakotaAEP.power')
-    prob.root.connect('w.weight', 'DakotaAEP.weights')
-    prob.root.connect('rho.frequency', 'DakotaAEP.frequency')
+    if method_dict['method'] == 'rect':
+        prob.root.add('AEPComp', RectStatistics(nDirections=n, method_dict=method_dict))#, promotes=['*'])  # No need to promote because of the explicit connection below
+    if method_dict['method'] == 'dakota':
+        prob.root.add('AEPComp', DakotaStatistics(nDirections=n, method_dict=method_dict))#, promotes=['*'])
+    prob.root.connect('p.power', 'AEPComp.power')
+    prob.root.connect('w.weight', 'AEPComp.weights')
     prob.setup()
     prob.run()
-    print 'AEP = ', (prob.root.DakotaAEP.unknowns['AEP'])
-    print 'power directions = ', (prob.root.DakotaAEP.params['power'])
-    print prob.root.DakotaAEP.params.keys()
-    # The DakotaAEP.power_directions key is not recognized
-    # J = prob.calc_gradient(['DakotaAEP.AEP'], ['DakotaAEP.power'])
-    # J = prob.calc_gradient(['DakotaAEP.AEP'], ['p.power'])
+    print 'AEP = ', (prob.root.AEPComp.unknowns['mean'])
+    print 'power = ', (prob.root.AEPComp.params['power'])
+    print prob.root.AEPComp.params.keys()
+    # J = prob.calc_gradient(['AEPComp.mean'], ['p.power'])  # I'm not sure why this returns zero
     # print 'power directions gradient = ', J
 
     # This check works

1d_dakota_uniform/statistics_convergence.py
@@ -27,7 +27,7 @@ def run():
     method_dict = {}
     method_dict['method']           = 'rect'
     method_dict['uncertain_var']    = 'direction'
-    method_dict['layout']             = 'optimized'
+    method_dict['layout']           = 'optimized'
 
     if method_dict['uncertain_var'] == 'speed':
         dist = distributions.getWeibull()
@@ -159,8 +159,10 @@ def getPoints(method_dict, n):
 
         # Get the weights associated with the points locations
 
-        # if method == 'rect':
-        w = getWeights(x, dx, dist)
+        if method == 'rect':
+            w = getWeights(x, dx, dist)
+        elif method == 'dakota':
+            w = wd
 
         # if method == 'dakota':
         #     # Logic to get the weights from integrating the pdf between the bins
@@ -277,23 +279,7 @@ def plot():
     # plt.show()
 
 
-def problem_set_up(windspeeds, winddirections, weights, method_dict=None):
-    """Set up wind farm problem.
-
-    Args:
-        windspeeds (np.array): wind speeds vector
-        winddirections (np.array): wind directions vector
-        weights (np.array): integration weights associated with the windspeeds and winddirections
-        method_dict (dict): UQ method and parameters for the UQ method
-
-    Returns:
-        prob (openMDAO problem class): The set up wind farm.
-
-    """
-
-
-
-
+def getLayout(layout='grid'):
     ### Set up the farm ###
 
     # Set up position of each turbine
@@ -310,7 +296,6 @@ def problem_set_up(windspeeds, winddirections, weights, method_dict=None):
     xlim = round_sig(np.max(turbineX))
     ylim = round_sig(np.max(turbineY))
 
-    layout = method_dict['layout']
     if layout == 'grid':
 
         # Grid farm (same number of turbines as Amalia 60)
@@ -363,19 +348,38 @@ def problem_set_up(windspeeds, winddirections, weights, method_dict=None):
     # For printing the location as an array
     # print turbineX
     # print turbineY
-    a = '['
-    for x in turbineX:
-        a = a + '%.0f' % x + ', '
-    print 'turbineX', a
-    a = '['
-    for y in turbineY:
-        a = a + '%.0f' % y + ', '
-    print 'turbineY', a
+    # a = '['
+    # for x in turbineX:
+    #     a = a + '%.0f' % x + ', '
+    # print 'turbineX', a
+    # a = '['
+    # for y in turbineY:
+    #     a = a + '%.0f' % y + ', '
+    # print 'turbineY', a
 
 
     # plt.figure()
     # plt.scatter(turbineX, turbineY)
     # plt.show()
+
+    return turbineX, turbineY
+
+
+def problem_set_up(windspeeds, winddirections, weights, method_dict=None):
+    """Set up wind farm problem.
+
+    Args:
+        windspeeds (np.array): wind speeds vector
+        winddirections (np.array): wind directions vector
+        weights (np.array): integration weights associated with the windspeeds and winddirections
+        method_dict (dict): UQ method and parameters for the UQ method
+
+    Returns:
+        prob (openMDAO problem class): The set up wind farm.
+
+    """
+
+    turbineX, turbineY = getLayout(method_dict['layout'])
 
     # turbine size and operating conditions
 