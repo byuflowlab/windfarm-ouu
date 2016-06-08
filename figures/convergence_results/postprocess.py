c876c930c8fed38b0c123a0e01ab2f02885d7d90
Santiago Padron <padronas@stanford.edu>
2016-06-08 12:40:43 -0700

    Added figures directory

---
+++ figures/convergence_results/postprocess.py
@@ -0,0 +1,418 @@
+
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+locations = np.genfromtxt('layout_grid.txt', delimiter='')
+tXg = locations[:, 0]
+tYg = locations[:, 1]
+
+locations = np.genfromtxt('layout_amalia.txt', delimiter='')
+tXa = locations[:, 0]
+tYa = locations[:, 1]
+
+locations = np.genfromtxt('layout_optimized.txt', delimiter='')
+tXo = locations[:, 0]
+tYo = locations[:, 1]
+
+locations = np.genfromtxt('layout_random.txt', delimiter='')
+tXr = locations[:, 0]
+tYr = locations[:, 1]
+
+# turbine locations
+tx = [tXg, tXa, tXo, tXr]
+ty = [tYg, tYa, tYo, tYr]
+
+
+# Figure 1 - Direction all 5 simulations
+# Different methods
+method = ['dir_rect.json', 'dir_dakota.json']
+# Different layouts
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+# Different simulations
+simulations = ['m2', 'm1', '0', '1', '2']
+# simulations = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+n = 45  # number of points to consider
+fig, ax = plt.subplots(4, 3, figsize=(12, 12), gridspec_kw={'width_ratios': [0.2,1,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['average']['mu'][-1]
+
+    for k, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        mu_ave = r[lay]['average']['mu'][:n]
+
+        # for j, sim in enumerate(simulations):
+        for sim in simulations:
+
+            # print lay, sim
+            s = r[lay][sim]['s'][:n]
+            mu = r[lay][sim]['mu'][:n]
+            # Plot each simulation
+            ax[i][k].plot(s, mu, label=sim)
+
+        # Plot the average values and the bounds
+        ax[i][k].plot(s, mu_ave, linewidth=2, color='k', label='average')
+        ax[i][k].plot(s, np.ones(len(s))*mu_base+0.01*mu_base, 'k--', label='1% bounds')
+        ax[i][k].plot(s, np.ones(len(s))*mu_base-0.01*mu_base, 'k--', label='1% bounds')
+
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    # ax[i][1].legend()
+ax[3][1].legend()
+ax[0][1].set_title('rectangle')
+ax[0][2].set_title('pce')
+ax[0][1].set_ylabel('AEP (GWhs)', rotation=90)
+ax[0][1].get_yaxis().set_label_coords(-0.1,0.5)
+ax[3][1].set_xlabel('# directions')
+ax[3][2].set_xlabel('# directions')
+
+
+# Figure 2 - Speed all 5 simulations for rect and 1 simulation for Dakota
+# Different methods
+method = ['speed_rect.json', 'speed_dakota.json']
+# Different layouts
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+
+# Different simulations
+simulations = ['m2', 'm1', '0', '1', '2']
+# simulations = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+
+n = 20  # number of points to consider
+fig, ax = plt.subplots(4, 3, figsize=(12, 12), gridspec_kw={'width_ratios': [0.2,1,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['average']['mu'][-1]
+
+    for k, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        mu_ave = r[lay]['average']['mu'][:n]
+
+        # for j, sim in enumerate(simulations):
+        for sim in simulations:
+
+            # print lay, sim
+            s = r[lay][sim]['s'][:n]
+            mu = r[lay][sim]['mu'][:n]
+            # Plot each simulation
+            ax[i][k].plot(s, mu, label=sim)
+
+        # Plot the average values and the bounds
+        # ax[i][k].plot(s, mu_ave, linewidth=2, color='k', label='average')
+        ax[i][k].plot(s, np.ones(len(s))*mu_base+0.01*mu_base, 'k--', label='1% bounds')
+        ax[i][k].plot(s, np.ones(len(s))*mu_base-0.01*mu_base, 'k--', label='1% bounds')
+
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    # ax[i][1].legend()
+ax[3][2].legend()
+ax[0][1].set_title('rectangle')
+ax[0][2].set_title('pce')
+ax[0][1].set_ylabel('AEP (GWhs)', rotation=90)
+ax[0][1].get_yaxis().set_label_coords(-0.1,0.5)
+ax[3][1].set_xlabel('# speeds')
+ax[3][2].set_xlabel('# speeds')
+
+
+
+# Figure 3 - Direction Mean, min, max
+
+# Different layouts
+
+method = ['dir_rect.json', 'dir_dakota.json']
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+
+n = 45  # number of points to consider
+
+fig, ax = plt.subplots(4, 3, figsize=(12, 12), gridspec_kw={'width_ratios': [0.2,1,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['average']['mu'][-1]
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    for j, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        # Baseline values for error bounds and mean values
+        s = r[lay]['average']['s'][:n]
+        mu_ave = r[lay]['average']['mu'][:n]
+
+        mu_max = r[lay]['max']['mu'][:n]
+        mu_min = r[lay]['min']['mu'][:n]
+
+        # Plot the average values and the bounds
+        ax[i][j].plot(s, mu_ave, label='average')
+        ax[i][j].plot(s, np.ones(len(s))*mu_base+0.01*mu_base, 'k--', label='1% bounds')
+        ax[i][j].plot(s, np.ones(len(s))*mu_base-0.01*mu_base, 'k--', label='1% bounds')
+
+        # Plot the min, max range
+        ax[i][j].fill_between(s, mu_min, mu_max, facecolor='blue', alpha=0.2 )
+
+ax[0][1].set_title('rectangle')
+ax[0][2].set_title('pce')
+ax[0][1].set_ylabel('AEP (GWhs)', rotation=90)
+ax[0][1].get_yaxis().set_label_coords(-0.1,0.5)
+ax[3][1].set_xlabel('# directions')
+ax[3][2].set_xlabel('# directions')
+plt.savefig('Statistics_convergence_mean_min_max_direction.pdf', transparent=True)
+
+
+# Figure 4 - Speed Mean, min, max
+
+# Different layouts
+
+method = ['speed_rect.json', 'speed_dakota.json']
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+n = 20  # number of points to consider
+
+fig, ax = plt.subplots(4, 3, figsize=(12, 12), gridspec_kw={'width_ratios': [0.2,1,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['average']['mu'][-1]
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    for j, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        # Baseline values for error bounds and mean values
+        s = r[lay]['average']['s'][:n]
+        mu_ave = r[lay]['average']['mu'][:n]
+
+        mu_max = r[lay]['max']['mu'][:n]
+        mu_min = r[lay]['min']['mu'][:n]
+
+        # Plot the average values and the bounds
+        ax[i][j].plot(s, mu_ave, label='average')
+        ax[i][j].plot(s, np.ones(len(s))*mu_base+0.01*mu_base, 'k--', label='1% bounds')
+        ax[i][j].plot(s, np.ones(len(s))*mu_base-0.01*mu_base, 'k--', label='1% bounds')
+
+        # Plot the min, max range
+        ax[i][j].fill_between(s, mu_min, mu_max, facecolor='blue', alpha=0.2 )
+
+ax[0][1].set_title('rectangle')
+ax[0][2].set_title('pce')
+ax[0][1].set_ylabel('AEP (GWhs)', rotation=90)
+ax[0][1].get_yaxis().set_label_coords(-0.1,0.5)
+ax[3][1].set_xlabel('# speeds')
+ax[3][2].set_xlabel('# speeds')
+# plt.savefig('Statistics_convergence.pdf', transparent=True)
+
+
+# Figure 4b - Speed Mean convergence, doesn't average over starting points
+
+# Different layouts
+
+method = ['speed_rect.json', 'speed_dakota.json']
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+
+n = 20  # number of points to consider
+
+fig, ax = plt.subplots(4, 3, figsize=(12, 12), gridspec_kw={'width_ratios': [0.2,1,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['0']['mu'][-1]
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    for j, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        # Baseline values for error bounds and mean values
+        s = r[lay]['0']['s'][:n]
+        mu_ave = r[lay]['0']['mu'][:n]
+
+        # Plot the average values and the bounds
+        ax[i][j].plot(s, mu_ave, label='0')
+        ax[i][j].plot(s, np.ones(len(s))*mu_base+0.01*mu_base, 'k--', label='1% bounds')
+        ax[i][j].plot(s, np.ones(len(s))*mu_base-0.01*mu_base, 'k--', label='1% bounds')
+
+
+ax[0][1].set_title('rectangle')
+ax[0][2].set_title('pce')
+ax[0][1].set_ylabel('AEP (GWhs)', rotation=90)
+ax[0][1].get_yaxis().set_label_coords(-0.1,0.5)
+ax[3][1].set_xlabel('# speeds')
+ax[3][2].set_xlabel('# speeds')
+plt.savefig('Statistics_convergence_speed.pdf', transparent=True)
+
+
+# Figure 5 - Direction Mean Error
+
+# Different layouts
+
+method = ['dir_rect.json', 'dir_dakota.json']
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+
+n = 45  # number of points to consider
+
+fig, ax = plt.subplots(4, 2, figsize=(8, 12), gridspec_kw={'width_ratios': [0.2,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['average']['mu'][-1]
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    for j, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        # Baseline values for error bounds and mean values
+        s = r[lay]['average']['s'][:n]
+        mu_ave = np.array(r[lay]['average']['mu'][:n])
+
+        mu_max = np.array(r[lay]['max']['mu'][:n])
+        mu_min = np.array(r[lay]['min']['mu'][:n])
+
+        mu_err = np.abs((mu_ave-mu_base))/mu_base *100
+        mu_err_max = np.max((np.abs(mu_max-mu_base), np.abs(mu_min-mu_base)), axis=0)/mu_base *100
+        # Plot the average values and the bounds
+        ax[i][1].plot(s, mu_err) #, label=m.split('.')[0])
+        # ax[i][1].plot(s, mu_err_max, '--')
+        # ax[i][1].legend()
+        ax[i][1].legend(['rectangle', 'pce'], frameon=False)
+        # Plot the min, max range
+        # ax[i][1].fill_between(s, mu_min, mu_max, facecolor='blue', alpha=0.2 )
+
+ax[0][1].set_ylabel('% average error', rotation=90)
+ax[3][1].set_xlabel('# directions')
+
+#plt.savefig('Error_statistics_convergence.pdf')
+plt.savefig('MeanError_statistics_convergence_direction.pdf', transparent=True)
+
+
+# Figure 6 - Speed Error, doesn't average over starting points
+
+# Different layouts
+
+method = ['speed_rect.json', 'speed_dakota.json']
+# method = ['dakota_speed.json']
+layout = ['grid', 'amalia', 'optimized', 'lhs']
+# layout = ['grid', 'amalia', 'optimized', 'random']
+
+
+n = 25  # number of points to consider
+
+fig, ax = plt.subplots(4, 2, figsize=(8, 12), gridspec_kw={'width_ratios': [0.2,1]})
+
+for i, lay in enumerate(layout):
+    # Get the baseline
+    f = open(method[0], 'r')
+    r = json.load(f)
+    f.close()
+    mu_base = r[lay]['0']['mu'][-1]
+
+    # Plot the layout
+    ax[i][0].scatter(tx[i], ty[i])
+    ax[i][0].set_axis_off()
+    ax[i][0].set_xlim([-190, 3990])
+    ax[i][0].set_ylim([-245, 5145])
+    ax[i][0].set_aspect('equal')
+
+    for j, m in enumerate(method, 1):
+        f = open(m, 'r')
+        r = json.load(f)
+        f.close()
+
+        # Baseline values for error bounds and mean values
+        s = r[lay]['0']['s'][:n]
+        mu_ave = np.array(r[lay]['0']['mu'][:n])
+
+        mu_err = np.abs((mu_ave-mu_base))/mu_base *100
+        # Plot the average values and the bounds
+        ax[i][1].plot(s, mu_err) #, label=m.split('.')[0])
+        # ax[i][1].plot(s, mu_err_max, '--')
+        # ax[i][1].legend()
+        ax[i][1].legend(['rectangle', 'pce'], frameon=False)
+
+
+ax[0][1].set_ylabel('% average error', rotation=90)
+ax[3][1].set_xlabel('# speeds')
+
+plt.savefig('Error_statistics_convergence_speed.pdf', transparent=True)
+
+plt.show()
---
+++ figures/optimization_results/postprocess_combined.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+
+
+# Amalia baseline coordinates
+tXama = np.array([973, 1553, 2158, 721, 1290, 1895, 2515, 3136, 469, 1033, 1623, 2238, 2853, 3483, 217, 776, 1356, 1956, 2571, 3186, 3801, 519, 1094, 1684, 2284, 2894, 3503, 257, 822, 1406, 1996, 2601, 3201, 3781, 0, 560, 1129, 1714, 2309, 2904, 3468, 287, 852, 1432, 2006, 2596, 3156, 3710, 20, 575, 1139, 1719, 2284, 2843, 297, 852, 1427, 1986, 1124, 1684])
+tYama = np.array([10, 0, 20, 509, 494, 509, 539, 580, 1003, 978, 988, 1023, 1059, 1104, 1497, 1472, 1477, 1497, 1532, 1573, 1633, 1961, 1961, 1986, 2006, 2047, 2097, 2460, 2450, 2460, 2485, 2520, 2566, 2611, 2949, 2934, 2944, 2954, 2989, 3025, 3070, 3423, 3428, 3438, 3458, 3493, 3534, 3569, 3912, 3912, 3912, 3927, 3962, 3992, 4391, 4386, 4401, 4421, 4870, 4890])
+
+
+method = ['dakota_opt_muactual.json', 'rect_opt_muactual.json']
+samples = [['10', '20', '30', '40'], ['10', '20', '30', '40', '66', '100']]
+# values = ['std', 'obj', 'tX', 'tY', 'iter', 'mu', 'mu_actual']
+case = '0'
+fig, ax = plt.subplots(2, 7, figsize=(24,8))
+for j, m in enumerate(method):
+    # Load the json file with all the information
+    f = open(m, 'r')
+    r = json.load(f)
+    f.close()
+    for i, n in enumerate(samples[j], 1):
+        tX = r[n][case]['tX']
+        tY = r[n][case]['tY']
+        mu_actual = r[n][case]['mu_actual']
+        mu = r[n][case]['mu']
+        ax[j][i].scatter(tX, tY)
+        ax[j][i].set_axis_off()
+        ax[j][i].set_xlim([-190, 3990])
+        ax[j][i].set_ylim([-245, 5145])
+        ax[j][i].set_aspect('equal')
+        # towrite = n #+ ' - ' + str(int(round(mu_actual))) + ' ' + str(int(round(mu)))
+        # ax[j][i].set_title(towrite)
+        ax[1][i].text(1600, -700, n)
+        ax[j][i].text(3200, 5000, str(int(round(mu_actual))), fontsize=14)
+        ax[j][i].text(3200, 0, str(int(round(mu))))
+
+
+# Baseline plot
+ax[0][0].scatter(tXama, tYama)
+ax[0][0].set_axis_off()
+ax[0][0].set_xlim([-190, 3990])
+ax[0][0].set_ylim([-245, 5145])
+ax[0][0].set_aspect('equal')
+ax[1][0].scatter(tXama, tYama)
+ax[1][0].set_axis_off()
+ax[1][0].set_xlim([-190, 3990])
+ax[1][0].set_ylim([-245, 5145])
+ax[1][0].set_aspect('equal')
+
+# Clear the last grids
+ax[0][5].set_axis_off()
+ax[0][6].set_axis_off()
+
+
+# ax[0][0].text(3200, 5000, 'aloha')
+# ax[0][0].text(3200, 0, 'aloha')
+
+ax[0][4].text(4100, 5000, 'GWh - AEP of the layout as measured by using rectangular integration with n=100 directions')
+plt.savefig('optimizaton.pdf')
+
+plt.show()
