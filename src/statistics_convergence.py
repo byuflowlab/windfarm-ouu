c2c7c0661d745f0eff563946e565540895afc79b
Santiago Padron <padronas@stanford.edu>
2016-07-08 11:07:09 -0700

    Moved the loop of the statistics convergence into the main and outside of the run

--- src/statistics_convergence.py
+++ src/statistics_convergence.py
@@ -9,7 +9,7 @@ import distributions
 import windfarm_setup
 
 
-def run(method_dict):
+def run(method_dict, n):
     """
     method_dict = {}
     keys of method_dict:
@@ -25,135 +25,111 @@ def run(method_dict):
         Writes a json file 'record.json' with the run information.
     """
 
-    mean = []
-    std = []
-    samples = []
 
-    for n in range(5,6,1):
+    ### Set up the wind speeds and wind directions for the problem ###
 
-        ### Set up the wind speeds and wind directions for the problem ###
+    points, weights = windfarm_setup.getPoints(method_dict, n)
+    N = points.size  # actual number of samples
 
-        points, weights = windfarm_setup.getPoints(method_dict, n)
-        N = points.size  # actual number of samples
-
-        if method_dict['uncertain_var'] == 'speed':
-            # For wind speed
-            windspeeds = points
-            winddirections = np.ones(N)*225
-        elif method_dict['uncertain_var'] == 'direction':
-            # For wind direction
-            windspeeds = np.ones(N)*8
-            winddirections = points
-        else:
-            raise ValueError('unknown uncertain_var option "%s", valid options "speed" or "direction".' %method_dict['uncertain_var'])
+    if method_dict['uncertain_var'] == 'speed':
+        # For wind speed
+        windspeeds = points
+        winddirections = np.ones(N)*225
+    elif method_dict['uncertain_var'] == 'direction':
+        # For wind direction
+        windspeeds = np.ones(N)*8
+        winddirections = points
+    else:
+        raise ValueError('unknown uncertain_var option "%s", valid options "speed" or "direction".' %method_dict['uncertain_var'])
 
 
-        print 'Locations at which power is evaluated'
-        print '\twindspeed \t winddirection'
-        for i in range(N):
-            print i+1, '\t', '%.2f' % windspeeds[i], '\t', '%.2f' % winddirections[i]
+    print 'Locations at which power is evaluated'
+    print '\twindspeed \t winddirection'
+    for i in range(N):
+        print i+1, '\t', '%.2f' % windspeeds[i], '\t', '%.2f' % winddirections[i]
 
 
-        # Turbines layout
-        turbineX, turbineY = windfarm_setup.getLayout(method_dict['layout'])
+    # Turbines layout
+    turbineX, turbineY = windfarm_setup.getLayout(method_dict['layout'])
 
-        # turbine size and operating conditions
+    # turbine size and operating conditions
 
-        rotor_diameter = 126.4  # (m)
-        air_density = 1.1716    # kg/m^3
+    rotor_diameter = 126.4  # (m)
+    air_density = 1.1716    # kg/m^3
 
-        # initialize arrays for each turbine properties
-        nTurbs = turbineX.size
-        rotorDiameter = np.zeros(nTurbs)
-        axialInduction = np.zeros(nTurbs)
-        Ct = np.zeros(nTurbs)
-        Cp = np.zeros(nTurbs)
-        generator_efficiency = np.zeros(nTurbs)
-        yaw = np.zeros(nTurbs)
+    # initialize arrays for each turbine properties
+    nTurbs = turbineX.size
+    rotorDiameter = np.zeros(nTurbs)
+    axialInduction = np.zeros(nTurbs)
+    Ct = np.zeros(nTurbs)
+    Cp = np.zeros(nTurbs)
+    generator_efficiency = np.zeros(nTurbs)
+    yaw = np.zeros(nTurbs)
 
-        # define initial values
-        for turbI in range(nTurbs):
-            rotorDiameter[turbI] = rotor_diameter
-            axialInduction[turbI] = 1.0/3.0
-            Ct[turbI] = 4.0*axialInduction[turbI]*(1.0-axialInduction[turbI])
-            Cp[turbI] = 0.7737/0.944 * 4.0 * 1.0/3.0 * np.power((1 - 1.0/3.0), 2)
-            generator_efficiency[turbI] = 0.944
-            yaw[turbI] = 0.     # deg.
+    # define initial values
+    for turbI in range(nTurbs):
+        rotorDiameter[turbI] = rotor_diameter
+        axialInduction[turbI] = 1.0/3.0
+        Ct[turbI] = 4.0*axialInduction[turbI]*(1.0-axialInduction[turbI])
+        Cp[turbI] = 0.7737/0.944 * 4.0 * 1.0/3.0 * np.power((1 - 1.0/3.0), 2)
+        generator_efficiency[turbI] = 0.944
+        yaw[turbI] = 0.     # deg.
 
-        # initialize problem
-        prob = Problem(AEPGroup(nTurbines=nTurbs, nDirections=N,
-                                method_dict=method_dict))
-        prob.setup(check=False)
+    # initialize problem
+    prob = Problem(AEPGroup(nTurbines=nTurbs, nDirections=N,
+                            method_dict=method_dict))
+    prob.setup(check=False)
 
-        # assign initial values to variables
-        prob['windSpeeds'] = windspeeds
-        prob['windDirections'] = winddirections
-        prob['weights'] = weights
-        prob['rotorDiameter'] = rotorDiameter
-        prob['axialInduction'] = axialInduction
-        prob['generatorEfficiency'] = generator_efficiency
-        prob['air_density'] = air_density
-        prob['Ct_in'] = Ct
-        prob['Cp_in'] = Cp
+    # assign initial values to variables
+    prob['windSpeeds'] = windspeeds
+    prob['windDirections'] = winddirections
+    prob['weights'] = weights
+    prob['rotorDiameter'] = rotorDiameter
+    prob['axialInduction'] = axialInduction
+    prob['generatorEfficiency'] = generator_efficiency
+    prob['air_density'] = air_density
+    prob['Ct_in'] = Ct
+    prob['Cp_in'] = Cp
 
-        prob['turbineX'] = turbineX
-        prob['turbineY'] = turbineY
-        for direction_id in range(0, N):
-            prob['yaw%i' % direction_id] = yaw
+    prob['turbineX'] = turbineX
+    prob['turbineY'] = turbineY
+    for direction_id in range(0, N):
+        prob['yaw%i' % direction_id] = yaw
 
-        # Run the problem
-        prob.run()
+    # Run the problem
+    prob.run()
 
-        # print the results
-        mean_data = prob['mean']
-        std_data = prob['std']
-        print 'mean = ', mean_data/1e6, ' GWhrs'
-        print 'std = ', std_data/1e6, ' GWhrs'
-
-        mean.append(mean_data/1e6)
-        std.append(std_data/1e6)
-        samples.append(n)
-
-
-    # Save a record of the run
+    # print the results
+    mean_data = prob['mean']
+    std_data = prob['std']
+    factor = 1e6
+    print 'mean = ', mean_data/factor, ' GWhrs'
+    print 'std = ', std_data/factor, ' GWhrs'
     power = prob['power']
 
-    obj = {'mean': mean, 'std': std, 'samples': samples, 'winddirections': winddirections.tolist(),
-           'windspeeds': windspeeds.tolist(), 'power': power.tolist(),
-           'method': method_dict['method'], 'uncertain_variable': method_dict['uncertain_var'],
-           'layout': method_dict['layout']}
-    jsonfile = open('record.json','w')
-    json.dump(obj, jsonfile, indent=2)
-    jsonfile.close()
+    return mean_data/factor, std_data/factor, N, winddirections, windspeeds, power
 
-    # fig, ax = plt.subplots()
-    # #ax.plot(windspeeds, power)
-    # ax.plot(winddirections, power)
-    # ax.set_xlabel('wind speed (m/s)')
-    # ax.set_ylabel('power')
-    #
-    # plt.show()
 
 def plot():
     jsonfile = open('record.json','r')
     a = json.load(jsonfile)
-    print a
-    print type(a)
+    #print a
     print a.keys()
-    print json.dumps(a, indent=2)
+    # print json.dumps(a, indent=2)
 
-    # fig, ax = plt.subplots()
-    # ax.plot(windspeeds, power)
-    # ax.set_xlabel('wind speed (m/s)')
-    # ax.set_ylabel('power')
-    #
-    # fig, ax = plt.subplots()
-    # ax.plot(samples,mean)
-    # ax.set_xlabel('Number of Wind Directions')
-    # ax.set_ylabel('mean annual energy production')
-    # ax.set_title('Mean annual energy as a function of the Number of Wind Directions')
-    #
-    # plt.show()
+    fig, ax = plt.subplots()
+    ax.plot(a['winddirections'], a['power'])
+    # ax.plot(a['windspeeds'], a['power'])
+    ax.set_xlabel('wind directions (deg)')
+    ax.set_ylabel('power')
+
+    fig, ax = plt.subplots()
+    ax.plot(a['samples'], a['mean'])
+    ax.set_xlabel('Number of Wind Directions')
+    ax.set_ylabel('mean annual energy production')
+    ax.set_title('Mean annual energy as a function of the Number of Wind Directions')
+
+    plt.show()
 
 
 def get_args():
@@ -193,8 +169,29 @@ if __name__ == "__main__":
     else:
         raise ValueError('unknown uncertain_var option "%s", valid options "speed" or "direction".' %method_dict['uncertain_var'])
 
-    # Run the problem
-    run(method_dict)
+    # Run the problem multiple times for statistics convergence
+    mean = []
+    std = []
+    samples = []
+
+    # Depending on the case n can represent number of quadrature points, sparse grid level, expansion order
+    # n is roughly a surrogate for the number of samples
+    for n in range(5, 6, 1):
+
+        # Run the problem
+        mean_data, std_data, N, winddirections, windspeeds, power = run(method_dict, n)
+        mean.append(mean_data)
+        std.append(std_data)
+        samples.append(N)
+
+    # Save a record of the run
+
+    obj = {'mean': mean, 'std': std, 'samples': samples, 'winddirections': winddirections.tolist(),
+           'windspeeds': windspeeds.tolist(), 'power': power.tolist(),
+           'method': method_dict['method'], 'uncertain_variable': method_dict['uncertain_var'],
+           'layout': method_dict['layout']}
+    jsonfile = open('record.json','w')
+    json.dump(obj, jsonfile, indent=2)
+    jsonfile.close()
+
     # plot()
-
-
